7bf26c81266d7698efd8bb7a0d996a86cb223d63
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-24 23:18:44 +0100

    worked on the TweetsController

--- app/assets/javascripts/home/home.controller.js
+++ app/assets/javascripts/home/home.controller.js
@@ -6,8 +6,53 @@
     var vm = this;
 
     // callable methods on the vm
-    vm.name = 'Bruno';
-  }
+    // readability: you know what function you can use on the front end on the vm
+    // factory inspired pattern
+    vm.name = 'From the Tweets App - inside of the Home controller';
+    vm.getTweet = getTweet;
+    vm.createTweet = createTweet; // setup to be the value of the function, creating a callable attribute (like protected instance mthod you get access to)
+    vm.updateTweet = updateTweet;
+    vm.deleteTweet = deleteTweet;
+
+    // methods we'll instantiate the controller with
+    // upon entrance we want to activate
+    // this is not a vm method
+    activate();
+
+    // defined methods
+    function activate(){
+      getTweets(){
+
+      }
+    }
+
+    // called by activate
+    // http://localhost:3000/tweets JSON
+    function getTweets(){
+
+    }
+
+    // vm method
+    function getTweet(){
+
+    }
+
+    // vm method
+    function createTweet(){
+
+    }
+
+    // vm method
+    function updateTweet(){
+
+    }
+
+    // vm method
+    function deleteTweet(){
+
+    }
+
+  };
 
   angular
     .module('app')
---
+++ app/assets/javascripts/tweets/tweets.controller.js
@@ -0,0 +1,58 @@
+(function(){
+  'use strict';
+
+  // each time called by the route we call a new instance of the HomeController constructor function
+  function TweetsController() {
+    var vm = this;
+
+    // callable methods on the vm
+    // readability: you know what function you can use on the front end on the vm
+    // factory inspired pattern
+    vm.name = 'From the Tweets App - inside of the Tweets controller';
+    vm.getTweet = getTweet;
+    vm.createTweet = createTweet; // setup to be the value of the function, creating a callable attribute (like protected instance mthod you get access to)
+    vm.updateTweet = updateTweet;
+    vm.deleteTweet = deleteTweet;
+
+    // methods we'll instantiate the controller with
+    // upon entrance we want to activate
+    // this is not a vm method
+    activate();
+
+    // defined methods
+    function activate(){
+      getTweets()
+    }
+
+    // called by activate
+    // http://localhost:3000/tweets JSON
+    function getTweets(){
+
+    }
+
+    // vm method
+    function getTweet(){
+
+    }
+
+    // vm method
+    function createTweet(){
+
+    }
+
+    // vm method
+    function updateTweet(){
+
+    }
+
+    // vm method
+    function deleteTweet(){
+
+    }
+
+  };
+
+  angular
+    .module('app')
+    .controller('TweetsController', TweetsController);
+}());
