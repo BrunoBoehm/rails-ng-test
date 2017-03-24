25f18bc37231fbdd3a66c89ee104f511d601d5a5
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-24 22:46:50 +0100

    added a tweets controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,41 @@
+class TweetsController < ActionController::Base
+
+  def index
+    tweets = Tweet.all
+    render json: tweets
+  end
+
+  def create
+    tweet = Tweet.new(tweet_params)
+    if tweet.save
+      render json: tweet
+    else
+      render json: { errors: tweet.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    tweet = Tweet.find_by(id: params[:id])
+    if tweet.update(tweet_params)
+      render json: tweet
+    else
+      render json: { errors: tweet.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find_by(id: params[:id])
+    tweet.destroy
+  end
+
+  def show
+    tweet = Tweet.find_by(id: params[:id])
+    render json: tweet
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
+
+end
